291a2c34ffda6e322ab9ec639ff05551831ab228
Alex Preiss <alexspreiss@gmail.com>
2020-02-12 00:21:49 -0800

    connect to heroku db

--- src/database/models/index.js
+++ src/database/models/index.js
@@ -1,13 +1,20 @@
 import Sequelize from "sequelize"
 import "dotenv/config"
-const sequelize = new Sequelize(
-  process.env.DATABASE,
-  process.env.DATABASE_USER,
-  process.env.DATABASE_PASSWORD,
-  {
-    dialect: "postgres",
-  }
-)
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  dialectOptions: {
+    ssl: true,
+  },
+})
+//   {
+//   process.env.DATABASE_URL,
+// database:  process.env.DATABASE,
+//   username: process.env.DATABASE_USER,
+//   password: process.env.DATABASE_PASSWORD,
+//
+//     dialect: "postgres",
+//   }
 const models = {
   User: sequelize.import("./user"),
   Post: sequelize.import("./post"),
