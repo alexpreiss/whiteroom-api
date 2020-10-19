734d0591041b24688143c82378146219266783c8
Alex Preiss <alexspreiss@gmail.com>
2020-10-19 16:57:47 -0700

    Update config.js

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,15 +2,18 @@ require("dotenv").config()
 
 module.exports = {
   development: {
-    url: process.env.DEV_DATABASE_URL,
+    url:
+      "postgres://zswedyeqhelshk:1bc5cc10c960ccd1332fb9f88d62c8ca26206924c73218042046ca03422f2964@ec2-52-23-14-156.compute-1.amazonaws.com:5432/d6755ua88nvmus",
     dialect: "postgres",
   },
   test: {
-    url: process.env.TEST_DATABASE_URL,
+    url:
+      "postgres://zswedyeqhelshk:1bc5cc10c960ccd1332fb9f88d62c8ca26206924c73218042046ca03422f2964@ec2-52-23-14-156.compute-1.amazonaws.com:5432/d6755ua88nvmus",
     dialect: "postgres",
   },
   production: {
-    url: process.env.DATABASE_URL,
+    url:
+      "postgres://zswedyeqhelshk:1bc5cc10c960ccd1332fb9f88d62c8ca26206924c73218042046ca03422f2964@ec2-52-23-14-156.compute-1.amazonaws.com:5432/d6755ua88nvmus",
     dialect: "postgres",
   },
 }
