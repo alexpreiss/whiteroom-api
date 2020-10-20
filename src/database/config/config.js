4f230afa84387ef3207be8f85e306fc1d0900810
Alex Preiss <alexspreiss@gmail.com>
2020-10-19 17:00:10 -0700

    Revert "Update config.js"
    This reverts commit 734d0591041b24688143c82378146219266783c8.

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,18 +2,15 @@ require("dotenv").config()
 
 module.exports = {
   development: {
-    url:
-      "postgres://zswedyeqhelshk:1bc5cc10c960ccd1332fb9f88d62c8ca26206924c73218042046ca03422f2964@ec2-52-23-14-156.compute-1.amazonaws.com:5432/d6755ua88nvmus",
+    url: process.env.DEV_DATABASE_URL,
     dialect: "postgres",
   },
   test: {
-    url:
-      "postgres://zswedyeqhelshk:1bc5cc10c960ccd1332fb9f88d62c8ca26206924c73218042046ca03422f2964@ec2-52-23-14-156.compute-1.amazonaws.com:5432/d6755ua88nvmus",
+    url: process.env.TEST_DATABASE_URL,
     dialect: "postgres",
   },
   production: {
-    url:
-      "postgres://zswedyeqhelshk:1bc5cc10c960ccd1332fb9f88d62c8ca26206924c73218042046ca03422f2964@ec2-52-23-14-156.compute-1.amazonaws.com:5432/d6755ua88nvmus",
+    url: process.env.DATABASE_URL,
     dialect: "postgres",
   },
 }
